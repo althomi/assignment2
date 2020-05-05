ccca9f3af614307e5c0492e7e17eb39186d5a618
Ali <46216799+althomi@users.noreply.github.com>
2020-05-05 11:12:17 +0200

    new changes

--- TypeScriptAssignment/build/bundle.js
+++ TypeScriptAssignment/build/bundle.js
@@ -2,13 +2,40 @@
     'use strict';
 
     const firstDefaultForm = "First default form successfull";
-    class DefaultForm extends HTMLElement {
-        constructor() {
-            super();
-        }
+    /*const input1 = document.getElementById(`name`);
+    function typeName(name: string) {
+        return name;
     }
 
+    function inputChecker() {
+        if userinput = charsetString {
+            console.log("correct message")
+        }
+        else {
+            alert: wrong input
+        }
+
+    }*/
+    /*export class DefaultForm extends HTMLElement{
+        public  length: number = 8;
+        private charsetString: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        private charsetNumbers: number[] = [0,1,2,3,4,5,6,7,8,9];
+        private retVal: any = "";
+
+        constructor(inputClient: string){
+            super();
+            function typeName(){
+                private name = document.getElementById('name');
+
+
+            }
+
+        }
+
+    }*/
+
     console.log(firstDefaultForm);
-    customElements.define('my-beispiel', DefaultForm);
 
 }());
