9592879f1ca1adc88a1c34f0e55a8a8de1855c19
Ali <46216799+althomi@users.noreply.github.com>
2020-05-06 16:39:34 +0200

    disabled input field and cleaned up code in class usernamegenerator

--- TypeScriptAssignment/src/usernameGenerator.ts
+++ TypeScriptAssignment/src/usernameGenerator.ts
@@ -5,33 +5,14 @@ export class UsernameGenerator extends DefaultInputField {
     };
 
     var username = (document.getElementById('username') as HTMLInputElement).disabled = true;
-
-
-
-}
-
-
-
-    //const username = document.getElementById('username') as HTMLInputElement;
-    //this.username = document.getElementById('username').disabled = true;
-    /*let random: number = Math.floor(Math.random() *10);
-    let userNameNumber;
-
-
-
+    var random: number = Math.floor(Math.random() *10);
 
 
     function addInput(in1: string, in2: string, randomNumb: number) {
         return in1 + in2 +  randomNumb;
     }
 
-    //weist funktion typisiert einer variablen zu
-    let userNameGen: (int: string, int2: string, randomNumb: number) => any;
-
-
-
-    //add random number to username
-    function randomNumber() {
-    }
+    //typisierung function output
+    var userNameGen: (int: string, int2: string, randomNumb: number) => any;
 }
-*/
+
